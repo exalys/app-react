c6e80c62863767fdec6bfc16b8e6de1c81e39081
Frédéric Scholer <frederic.scholer@gmail.com>
2023-02-21 11:32:37 +0100

    next

--- src/components/login.component.jsx
+++ src/components/login.component.jsx
@@ -11,20 +11,17 @@ export const Login = () => {
   }
   
   return (<>
-  
     <h1>Bienvenue sur notre App</h1>
     <p>Tout d'abord, identifiez-vous :</p>
-
     <form className="row" onSubmit={handleSubmit}>
       <div className="col-md-6">
-        <input type="text" className="form-control" nbame="inputLogin" id="inputLogin" placeholder="Entrer votre identifiant" value={identifiant} />
+        <input type="text" className="form-control" nbame="inputLogin" id="inputLogin" placeholder="Entrer votre identifiant" onChange={(e) => setIdentifiant(e.target.value)} value={identifiant} />
       </div>
       <div className="col-md-6">
-      <button type="submit" className="btn btn-primary">S'identifier</button>
+      <button type="submit" className="btn btn-primary">Connexion</button>
       </div>
     </form>
-  
-  </>)
+    </>)
 
 }
 Login.defaultProps = {
