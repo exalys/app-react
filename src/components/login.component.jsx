05df0e9f34e146022c4c8904107ba5b2b3c17459
Frédéric Scholer <frederic.scholer@gmail.com>
2023-02-21 15:45:19 +0100

    end of 1st day

--- src/components/login.component.jsx
+++ src/components/login.component.jsx
@@ -13,7 +13,7 @@ export const Login = () => {
   return (<>
     <h1>Bienvenue sur notre App</h1>
     <p>Tout d'abord, identifiez-vous :</p>
-    <form className="row" onSubmit={() => onLogin(identifiant)}>
+    <form className="row" onSubmit={() => onLogin()}>
       <div className="col-md-6">
         <input type="text" className="form-control" name="inputLogin" id="inputLogin" placeholder="Entrer votre identifiant" onChange={(e) => setIdentifiant(e.target.value)} value={identifiant} />
       </div>
