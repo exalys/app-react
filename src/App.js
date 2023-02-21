bd7d61ee45ec2e6b20a1850b682c286126f87f06
Exalys <dev.exalys@gmail.com>
2023-02-21 15:40:43 +0100

    9

--- src/App.js
+++ src/App.js
@@ -4,22 +4,6 @@ import {Login} from './components/login.component'
 import CategoriesList from './components/categories/categories.component'
 import CategoriesAjax from './components/categories/categories.ajax'
 
-const categories = [
-  {id:1 , 
-  name:'cat1',
-  count: 4,
-  lastUpdate: 1676974378 },
-  {id:2 , 
-    name:'nom',
-    count: 1,
-    lastUpdate: 1676974378 }
-]
-
-const handleLogin = (identifiant) => {
-  console.log(identifiant)
-}
-
-
 function App() {
   const handleDisplay = (id) => {
     console.log(id)
@@ -27,11 +11,8 @@ function App() {
   return (
     <div className='container'>
       <HeaderContainer />
-      <Login onLogin={handleLogin}/>
-      {/* <h2>Salut {identifiant}</h2> */}
-      {/* <CategoriesList categories={categories} onDisplayItem={handleDisplay}/> */}
+      <Login/>
       <CategoriesAjax onDisplayItem={handleDisplay} />
-
     </div>
   )
 }
