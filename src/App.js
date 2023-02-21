1dbc1a4b0f96d070c0c1ce0d7dbc35aa10d056bf
Exalys <dev.exalys@gmail.com>
2023-02-21 15:22:49 +0100

    8

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import HeaderContainer from './containers/header/header.container'
 import {Login} from './components/login.component'
 import CategoriesList from './components/categories/categories.component'
+import CategoriesAjax from './components/categories/categories.ajax'
 
 const categories = [
   {id:1 , 
@@ -18,17 +19,18 @@ const handleLogin = (identifiant) => {
   console.log(identifiant)
 }
 
-const handleDisplay = (id) => {
-  console.log(id)
-}
 
 function App() {
+  const handleDisplay = (id) => {
+    console.log(id)
+  }
   return (
     <div className='container'>
       <HeaderContainer />
       <Login onLogin={handleLogin}/>
       {/* <h2>Salut {identifiant}</h2> */}
-      <CategoriesList categories={categories} onDisplayItem={handleDisplay}/>
+      {/* <CategoriesList categories={categories} onDisplayItem={handleDisplay}/> */}
+      <CategoriesAjax onDisplayItem={handleDisplay} />
 
     </div>
   )
